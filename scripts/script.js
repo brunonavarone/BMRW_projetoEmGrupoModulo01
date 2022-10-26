567a87d058070c8b319bcb1bee1577615205905c
Bruno Henrique <brunohenrique.travessa@gmail.com>
2022-10-26 11:19:26 -0300

    add mais alteracoes no formulario

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,24 @@
-console.log("Olá Mundo!")
+function verifcaNome()
+{
+    nome = document.getElementById("primeiroNome").value;
+    
+}
+
+
+function validaEmail()
+{
+    email = document.getElementById("email").value;
+    while(true)
+    {
+        if(email.match(/@/))
+        {
+            break;
+        }
+        else
+        {
+            alert("Você digitou um e-mail inválido!")
+            //email = prompt("Digite um e-mail válido: ");
+        }
+
+    }
+}
