49d5f77ca3b3a46ec8452ef6eea0b2550438715b
Navarone <brunohenrique.travessa@gmail.com>
2022-10-27 19:49:15 -0300

    add carrossel

---
+++ scripts/script_carrossel.js
@@ -0,0 +1,20 @@
+contador = 1;
+//verificando se o radio1 foi "apertado"
+document.getElementById("radio1").checked = true;
+
+//funcao que chama uma funcao com um intervalo de tempo passado como parametro 
+setInterval( function(){
+proximaImagem();
+}, 4000)
+
+//funcao que trocar a imagem de forma automática 
+function proximaImagem()
+{
+    contador++;
+    if(contador >5)
+    {
+        contador =1;
+    }
+
+    document.getElementById("radio"+contador).checked = true;
+}
