311fa5e764d21b02cef3a432f5ecb6b18dc27a38
Navarone <brunohenrique.travessa@gmail.com>
2022-10-26 21:29:01 -0300

    alteracoes nas validacoes de formulario_04

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,73 @@
-function verifcaNome()
+const formulario = document.getElementById('form');
+const primeiroNome = document.getElementById('primeiroNome');
+const sobreNome = document.getElementById('sobreNome');
+const email = document.getElementById('email');
+const celular = document.getElementById('numero');
+
+
+//resetando o compartamento padrao da pagina de formulários
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    verificaInputs();
+})
+
+function verificaInputs()
 {
-    nome = document.getElementById("primeiroNome").value;
-    
+    //funcao .trim() do js remove todos os espaçamentos em branco, retornando apenas o que foi digitado
+     const valorPrimeiroNome = primeiroNome.value.trim();
+     const valorSobreNome = sobreNome.value.trim();
+     const valorEmail = email.value.trim();
+     const valorCelular = celular.value.trim();
+
+    //verificando se o usuario digitou alguma coisa
+     if(valorPrimeiroNome === '')
+     {
+        validandoErro(primeiroNome);
+     }
+     else
+     {
+        validandoSucesso(primeiroNome);
+     }
+
+     if(valorSobreNome === '')
+     {
+        validandoErro(sobreNome);
+     }
+     else
+     {
+        validandoSucesso(sobreNome);
+     }
+
+     if(valorEmail === '')
+     {
+        validandoErro(email);
+     }
+     else
+     {
+        validandoSucesso(email);
+     }
+
+     if(valorCelular === '')
+     {
+        validandoErro(celular);
+     }
+     else
+     {
+        validandoSucesso(celular)
+     }
 }
 
-
-function validaEmail()
+function validandoErro(input)
 {
-    email = document.getElementById("email").value;
-    while(true)
-    {
-        if(email.match(/@/))
-        {
-            break;
-        }
-        else
-        {
-            alert("Você digitou um e-mail inválido!")
-            //email = prompt("Digite um e-mail válido: ");
-        }
+    const caixaInput = input.parentElement;
 
-    }
+    caixaInput.className = "caixa-input erro";
+}
+
+function validandoSucesso(input)
+{
+    const caixaInput = input.parentElement;
+
+    caixaInput.className = "caixa-input sucesso";
 }
